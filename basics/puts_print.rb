10a33b5accf0ae6b33e777495ebc1d42e0770b0c
Atique Akhtar <atiqueakhtar786@gmail.com>
2021-12-16 13:09:26 +0530

    puts print in ruby

---
+++ basics/puts_print.rb
@@ -0,0 +1,24 @@
+# puts takes us to the new line after printing whereas print does not.
+puts "Hello Atique!!"
+print "Hello World!"
+puts ""
+print "Atique "
+print "is cool"
+puts ""
+
+# Output :
+# Hello Atique!!
+# Hello World!
+# Atique is cool
+
+# triangle with puts
+puts "   /|"
+puts "  / |"
+puts " /  |"
+puts "/___|"
+
+# Output :
+#    /|
+#   / |
+#  /  |
+# /___|
