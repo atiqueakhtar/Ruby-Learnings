132f74be844cbb9e59c59ca69c18d02a37f90033
Atique Akhtar <atiqueakhtar786@gmail.com>
2021-12-17 14:28:14 +0530

    covered arrays

---
+++ basics/05_arrays.rb
@@ -0,0 +1,20 @@
+# Declaring and initializing an array:
+friends = Array["Adarsh", "Tauseef", "Akil", "Sohail"]
+friends[2] = "Dwight"
+print(friends[0,3])
+# ["Adarsh", "Tauseef", "Dwight"]
+
+# Declaring an array but not initializing in beginning:
+mates = Array.new
+mates[1] = "Thippu";
+mates[4] = "Vinod"
+print(mates)
+# [nil, "Thippu", nil, nil, "Vinod"]
+puts(mates.include? "Thippu") #true
+puts(mates.include? nil) #true
+
+# Some Array methods:
+print(friends.reverse())
+# ["Sohail", "Dwight", "Tauseef", "Adarsh"]
+print(friends.sort())
+# ["Adarsh", "Dwight", "Sohail", "Tauseef"]
