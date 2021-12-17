7fa0e2cc507a8341233de97ee5c01d4a0f353ba6
Atique Akhtar <atiqueakhtar786@gmail.com>
2021-12-17 17:39:15 +0530

    covered methods

--- basics/07_methods.rb
+++ basics/07_methods.rb
@@ -10,3 +10,16 @@ def cube(num)
     return num*num*num, "Hello", "Hi" #can return multiple values
 end
 puts(cube(2)[1]) #Hello printed as we accessed 1th index from the returned array
+
+# get the greatest of the 3:
+def getGreatest(arr)
+    if arr[0]>arr[1] and arr[0]>arr[2]
+        return arr[0]
+    elsif arr[1]>arr[0] and arr[1]>arr[0]
+        return arr[1]
+    else
+        return arr[2]
+    end
+end
+arr = [43,53,34]
+puts(getGreatest(arr))
