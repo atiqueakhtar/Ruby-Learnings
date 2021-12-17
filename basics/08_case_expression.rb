8b600074fa1064ba6704357c9f0d8b778ba57443
Atique Akhtar <atiqueakhtar786@gmail.com>
2021-12-17 20:06:40 +0530

    covered case expr

---
+++ basics/08_case_expression.rb
@@ -0,0 +1,26 @@
+def getDayName(day)
+
+    case day
+    when "mon"
+        day_name = "Monday"
+    when "tue"
+        day_name = "Tuesday"
+    when "wed"
+        day_name = "Wednesday"
+    when "thu"
+        day_name = "Thursday"
+    when "fri"
+        day_name = "Friday"
+    when "sat"
+        day_name = "Saturday"
+    when "sun"
+        day_name = "Sunday"
+    else
+        day_name = "Invalid Abrv."
+    end
+
+    return day_name
+end
+
+puts(getDayName("wed"))
+puts(getDayName("fri"))
