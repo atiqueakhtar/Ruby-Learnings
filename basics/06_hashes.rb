57bc7f6a10de34f5338117f6efc5131107bb89f7
Atique Akhtar <atiqueakhtar786@gmail.com>
2021-12-17 14:30:09 +0530

    covered hashes

---
+++ basics/06_hashes.rb
@@ -0,0 +1,13 @@
+# in a hash we can store data in a key-value pair
+# only unique keys allowed
+states = {
+    "Jharkhand" => "JHR",
+    "Assam" => "ASM",
+    "Hyderabad" => "HYD"
+}
+puts(states)
+# {"Jharkhand"=>"JHR", "Assam"=>"ASM", "Hyderabad"=>"HYD"}
+puts(states["Assam"])
+# ASM
+
+# Another way for representing hashes
